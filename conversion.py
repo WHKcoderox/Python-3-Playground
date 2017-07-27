9feedf6c8000a7bac341a66dbbf8d8f01be103d6
WHK <weehekun@gmail.com>
2017-07-27 12:47:53 +0800

    Update conversion.py

--- conversion.py
+++ conversion.py
@@ -6,6 +6,7 @@ def d2b(n):
         n //= NEWBASE
     for digit in ans[::-1]:
         print(digit,end='')
+    return ans
 
 def b2d(n):
     OLDBASE = 2
@@ -14,6 +15,7 @@ def b2d(n):
     for digit in range(len(n)):
         ans += n[digit]*OLDBASE**(len(n)-digit-1)
     print(ans)
+    return ans
 
 def d2o(n):
     NEWBASE = 8
@@ -23,6 +25,7 @@ def d2o(n):
         n //= NEWBASE
     for digit in ans[::-1]:
         print(digit,end='')
+    return ans
 
 def o2d(n):
     OLDBASE = 8
@@ -31,6 +34,7 @@ def o2d(n):
     for digit in range(len(n)):
         ans += n[digit]*OLDBASE**(len(n)-digit-1)
     print(ans)
+    return ans
 
 def interpret(digit):
     maps = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -44,6 +48,7 @@ def d2h(n):
         n //= NEWBASE
     for digit in ans[::-1]:
         print(digit,end='')
+    return ans
 
 def h2d(n):
     OLDBASE = 16
@@ -54,6 +59,7 @@ def h2d(n):
     for digit in range(len(n)):
         ans += maps[n[digit]]*OLDBASE**(len(n)-digit-1)
     print(ans)
+    return ans
 
 # base-n number class
 class Base_n:
@@ -108,6 +114,7 @@ def b2o(n):
     for digit in range(len(n)):
         ans.add(n[digit]*OLDBASE**(len(n)-digit-1))
     print(ans.getValueString())
+    return ans.getValueString()
 
 def o2b(n):
     OLDBASE = 8
@@ -117,6 +124,7 @@ def o2b(n):
     for digit in range(len(n)):
         ans.add(n[digit]*OLDBASE**(len(n)-digit-1))
     print(ans.getValueString())
+    return ans
 
 def b2h(n):
     OLDBASE = 2
@@ -126,6 +134,7 @@ def b2h(n):
     for digit in range(len(n)):
         ans.add(n[digit]*OLDBASE**(len(n)-digit-1))
     print(ans.getValueString())
+    return ans
 
 def h2b(n):
     OLDBASE = 16
@@ -137,6 +146,7 @@ def h2b(n):
     for digit in range(len(n)):
         ans.add(maps[n[digit]]*OLDBASE**(len(n)-digit-1))
     print(ans.getValueString())
+    return ans
 
 def o2h(n):
     OLDBASE = 8
@@ -148,6 +158,7 @@ def o2h(n):
     for digit in range(len(n)):
         ans.add(maps[n[digit]]*OLDBASE**(len(n)-digit-1))
     print(ans.getValueString())
+    return ans
 
 def h2o(n):
     OLDBASE = 16
@@ -159,6 +170,7 @@ def h2o(n):
     for digit in range(len(n)):
         ans.add(maps[n[digit]]*OLDBASE**(len(n)-digit-1))
     print(ans.getValueString())
+    return ans
 
 def n2m(n, m, num):
     OLDBASE = n
@@ -171,15 +183,19 @@ def n2m(n, m, num):
         ans.add(maps[num[digit]]*OLDBASE**(len(num)-digit-1))
         print(ans.getValueString())
     print(ans.getValueString())
+    return ans
 
 # binary mapping method
 def b2omap(n):
-    maps = [ '0'*(3-len(d2b(x))) + d2b(x) for x in range(8) ]
+    maps = { '0'*(3-len(d2b(x))) + ''.join(d2b(x)) : x for x in range(8) }
     digit = 0
     length = len(n)
     ans = []
     while digit < length:
         if digit + 2 >= length:
-            # 3-length-digit-1
-
+            n += (3 - (length - digit - 1))*'0'
+        ans.append(maps[ n[digit:digit+3] ])
+    for digit in ans:
+        print(digit, end='')
+    return ans
 
